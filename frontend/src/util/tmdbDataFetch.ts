ac6e6f2f7696b67d73fcf7482777dbea2e724f19
Max London <grahamluk66@gmail.com>
2025-02-01 15:45:59 +0000

    Use and suspence for detail loading

--- frontend/src/util/tmdbDataFetch.ts
+++ frontend/src/util/tmdbDataFetch.ts
@@ -1,32 +1,14 @@
-import { useEffect, useState } from "react";
-
-const useDataFetch = <T>(url: string) => {
-  const [data, setData] = useState<T | null>(null);
-  const [loading, setLoading] = useState<boolean>(true);
-  const [error, setError] = useState<string | null>(null);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch(url, {
-          headers: {
-            accept: "application/json",
-            Authorization: `Bearer ${import.meta.env.VITE_TMDB_API_KEY}`,
-          },
-        });
-        const data = await response.json();
-        setData(data);
-      } catch (error) {
-        setError(error as string);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchData();
-  }, [url]);
-
-  return { data, loading, error };
+export const useTMDBDataFetch = async <T>(
+  url: string,
+  config?: RequestInit,
+): Promise<T> => {
+  // Fetches data from the given URL and returns it as a promise
+  const response = await fetch(url, {
+    ...config,
+    headers: {
+      accept: "application/json",
+      Authorization: `Bearer ${import.meta.env.VITE_TMDB_API_KEY}`,
+    },
+  });
+  return response.json();
 };
-
-export default useDataFetch;
